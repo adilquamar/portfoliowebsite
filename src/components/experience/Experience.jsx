8c53440c6344a0914c97dd6e2c1cca9acac05701
Adil Quamar <adil.quamar14@gmail.com>
2023-08-03 23:30:13 -0700

    experience completed

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './experience.css'
+import {BsFillPatchCheckFill} from 'react-icons/bs'
 
 const Experience = () => {
   return (
@@ -7,7 +8,69 @@ const Experience = () => {
       <h5>Skills I Have</h5>
       <h2>My Experience</h2>
 
-      
+      <div className="container experience__container">
+        <div className="experience__software">
+          <h3>Software Development</h3>
+          <div className="div experience__content">
+            <article className='experience__details'>
+              <BsFillPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>Java</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsFillPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>Python</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsFillPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>C++</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsFillPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>React</h4>
+                <small className='text-light'>Novice</small>
+              </div>
+            </article>
+          </div>
+        </div>
+        {/* END OF SOFTWARE */}
+
+        <div className="experience__hardware">
+          <h3>Embedded & Hardware</h3>
+            <div className="div experience__content">
+              <article className='experience__details'>
+                <BsFillPatchCheckFill className='experience__details-icon'/>
+                <div>
+                  <h4>Verilog/VHDL</h4>
+                  <small className='text-light'>Intermediate</small>
+                </div>  
+              </article>
+              <article className='experience__details'>
+                <BsFillPatchCheckFill className='experience__details-icon'/>
+                <div>
+                  <h4>ROS/MAVROS</h4>
+                  <small className='text-light'>Intermediate</small>
+                </div> 
+              </article>
+              <article className='experience__details'>
+                <BsFillPatchCheckFill className='experience__details-icon'/>
+                <div>
+                  <h4>C</h4>
+                  <small className='text-light'>Intermediate</small>
+                </div> 
+              </article>
+            </div>
+        </div>
+      </div>
     </section>
   )
 }
