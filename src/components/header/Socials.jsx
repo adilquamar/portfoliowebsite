f7e600174db919383f33ed4e09a7acf36b1ddfa4
Adil Quamar <adil.quamar14@gmail.com>
2023-08-11 13:27:21 -0700

    increase social meadia icon size header

--- src/components/header/Socials.jsx
+++ src/components/header/Socials.jsx
@@ -5,8 +5,8 @@ import {FaGithub} from 'react-icons/fa'
 const Socials = () => {
   return (
     <div className='header__socials'>
-        <a href='https://www.linkedin.com/in/aquamar/' target='_blank' rel='noopener noreferrer'><BsLinkedin/></a>
-        <a href='https://github.com/adilquamar/' target='_blank' rel='noopener noreferrer'><FaGithub/></a>
+        <a href='https://www.linkedin.com/in/aquamar/' target='_blank' rel='noopener noreferrer'><BsLinkedin size={23}/></a>
+        <a href='https://github.com/adilquamar/' target='_blank' rel='noopener noreferrer'><FaGithub size={23}/></a>
     </div>
   )
 }
